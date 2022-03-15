1ca462ffbdecddf757e568d0346cf8ec2c3090e5
IvapPadalka <ipadalka@griddynamics.com>
2022-03-15 19:04:45 +0200

    Fixed

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
-    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
-        remindName();
-        guessAge();
-        count();
-        test();
-        end();
+        Main main = new Main();
+        main.greet("Aid", "2018");
+        main.remindName();
+        main.guessAge();
+        main.count();
+        main.test();
+        main.end();
     }
 
-    static void greet(String assistantName, String birthYear) {
-        System.out.println("Hello! My name is " + assistantName + ".");
-        System.out.println("I was created in " + birthYear + ".");
-        System.out.println("Please, remind me your name.");
+    void greet(String assistantName, String birthYear) {
+        System.out.println(" Hello! My name is " + assistantName + ".\n" +
+                " I was created in " + birthYear + ".\n" +
+                " Please, remind me your name.");
     }
 
-    static void remindName() {
+    void remindName() {
         String name = scanner.nextLine();
-        System.out.println("What a great name you have, " + name + "!");
+        System.out.println(" What a great name you have, " + name + "!");
     }
 
-    static void guessAge() {
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        int rem3 = scanner.nextInt();
-        int rem5 = scanner.nextInt();
-        int rem7 = scanner.nextInt();
-        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    void guessAge() {
+        int age;
+        System.out.println(" Let me guess your age.\n " +
+                "Enter remainders of dividing your age by 3, 5 and 7.");
+        age = (scanner.nextInt() * 70 + scanner.nextInt() * 21 + scanner.nextInt() * 15) % 105;
+        System.out.println(" Your age is " + age + "; that's a good time to start programming!");
     }
 
-    static void count() {
-        System.out.println("Now I will prove to you that I can count to any number you want.");
-        int num = scanner.nextInt();
+    void count() {
+        int num;
+        System.out.println(" Now I will prove to you that I can count to any number you want.");
+        num = scanner.nextInt();
         for (int i = 0; i <= num; i++) {
             System.out.printf("%d!\n", i);
         }
     }
 
-    static void test() {
-        System.out.println("Let's test your programming knowledge.");
-        System.out.println("Why do we use methods?");
-        System.out.println("1. To repeat a statement multiple times.");
-        System.out.println("2. To decompose a program into several small subroutines.");
-        System.out.println("3. To determine the execution time of a program.");
-        System.out.println("4. To interrupt the execution of a program.");
-        while (true){
-        int swither = scanner.nextInt();
-        switch (swither){
-            case 1:{
-
+    void test() {
+        int switcher;
+        System.out.println(" Let's test your programming knowledge.\n " +
+                "Why do we use methods?\n " +
+                "1. To repeat a statement multiple times.\n " +
+                "2. To decompose a program into several small subroutines.\n " +
+                "3. To determine the execution time of a program.\n " +
+                "4. To interrupt the execution of a program.");
+        loop: while (true){
+        switcher = scanner.nextInt();
+        switch (switcher){
+            default:{
+                System.out.println(" Please, try again.");
+                break;
             }
-            case 2:{
-                end();
-                System.exit(0);
-            }
-            case 3:{
-
-            }
-            case 4:{
-                System.out.println("Please, try again.");
+            case 3,4:{
+                new Main().end();
+                break loop;
             }
         }
         }
     }
 
-    static void end() {
-        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+    void end() {
+        System.out.println(" Congratulations, have a nice day!");
     }
 }
